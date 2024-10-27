7515cf0555703b93452899f5567de3f4244cc0e2
wiyco <72733890+wiyco@users.noreply.github.com>
2024-10-27 13:54:23 +0900

    refactor(cn): rewrite param type

--- app/utils/cn.ts
+++ app/utils/cn.ts
@@ -1,6 +1,8 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...classNames: (string | null | undefined)[]) {
+export function cn(
+  ...classNames: Parameters<typeof clsx> & Parameters<typeof twMerge>
+) {
   return twMerge(clsx(classNames));
 }
