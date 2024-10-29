743ff517674b5447a9eef4dd73d11c3b8b70acf2
wiyco <72733890+wiyco@users.noreply.github.com>
2024-10-29 23:20:38 +0900

    refactor(cn): strict return type

--- app/utils/cn.ts
+++ app/utils/cn.ts
@@ -3,6 +3,6 @@ import { twMerge } from "tailwind-merge";
 
 export function cn(
   ...classNames: Parameters<typeof clsx> & Parameters<typeof twMerge>
-) {
+): string {
   return twMerge(clsx(classNames));
 }
